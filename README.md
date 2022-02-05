f9bcfb9edcbb8b0871c4ca788394738c7b3fc3d6
Mrlolpro <leonardoollero14@gmail.com>
2022-02-05 11:40:18 +0100

    0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # web.cli
-A command line interface to init your web oroyect
+A command line interface to init your web proyect
+
+# Installation
+```
+$ npm i @mrlol/web.cli -g
+```
+
+# Usage
+```
+$ web
+```
--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ async function frontendSelector() {
         type: "list",
         name: "frontend",
         message: "What frontend framework do you want to use?",
-        choices: ["react", "vue", "angular"],
+        choices: ["react", "vue", "angular", "next", "svelte"],
         },
     ]);
     frontend = frontendPrompt.frontend;
@@ -71,7 +71,7 @@ async function backendSelector() {
         type: "list",
         name: "backend",
         message: "What backend framework do you want to use?",
-        choices: ["express", "nest"],
+        choices: ["express", "nest", "fastify", "koa"],
         },
     ]);
     backend = backendPrompt.backend;
@@ -131,47 +131,78 @@ async function getConfig() {
 }
 async function initProject() {
     if(frontend) {
-        if(fontent == "react") {
-            await load(1000, "Starting react project...");
-            await exec(`npx create-react-app client`);
+        if(frontend == "react") {
+            await load(100, "Starting react project with create-react-app...");
+            await exec('npm install -g create-react-app');
+            await load(15000, 'Waiting for create-react-app to finish installation...');
+            await exec(`create-react-app client`);
+        }
+        if(frontend == "vue") {
+            await load(100, "Starting vue project with vue-cli...");
+            await exec('npm install -g vue-cli');
+            await load(15000, 'Waiting for vue-cli to finish installation...');
+            await exec(`vue init webpack client`);
+        }
+        if(frontend == "angular") {
+            await load(100, "Starting angular project with ng new...");
+            await exec('npm install -g @angular/cli');
+            await load(15000, 'Waiting for ng new to finish installation...');
+            await exec(`ng new client`);
+        }
+        if(frontend == "next") {
+            await load(100, "Starting next project with next create...");
+            await exec('npm install -g next');
+            await load(15000, 'Waiting for next create to finish installation...');
+            await exec(`next create client`);
+        }
+        if(frontend == "svelte") {
+            await load(100, "Starting svelte project with svelte init...");
+            await exec('npm install -g svelte');
+            await load(15000, 'Waiting for svelte init to finish installation...');
+            await exec(`svelte init client`);
         }
     }
     if(backend) {
         if(backend == "express") {
-            await load(100, "Creating server package.json file...")
-            fs.writeFileSync("./server/package.json", '{\n  "name": "server",\n  "version": "1.0.0",\n  "description": "",\n  "main": "src/index.js",\n  "scripts": {},\n  "author": "",\n  "license": "ISC"\n}');
             await load(100, "Creating server folder...")
             fs.mkdirSync("./server");
             await load(100, "Creating server src folder...")
             fs.mkdirSync("./server/src");
+            await load(100, "Creating server package.json file...")
+            fs.writeFileSync("./server/package.json", '{\n  "name": "server",\n  "version": "1.0.0",\n  "description": "",\n  "main": "src/index.js",\n  "scripts": {},\n  "author": "",\n  "license": "ISC"\n}');
+            await load(100, "Creating server .env file...")
+            fs.writeFileSync("./server/.env", 'NODE_ENV=development\nPORT=3000');
+            await load(100, "Creating server index.js file...")
+            fs.writeFileSync("./server/src/index.js", `const express = require("express");\nconst app = express();\nconst morgan = require("morgan");\nrequire("dotenv").config();\n\napp.use(morgan("dev"));\napp.use(express.json());\napp.use(express.urlencoded({ extended: true }));\napp.set("PORT", process.env.PORT || 3000);\napp.use("/", require("./routes/routes"));\n\napp.listen(app.get("PORT"), () => {\n    console.log("Server running on port " + app.get("PORT"));\n});`);
             await load(100, "Creating server src/routes folder...")
             fs.mkdirSync("./server/src/routes");
             await load(100, "Creating server src/controllers folder...")
             fs.mkdirSync("./server/src/controllers");
-            await load(100, "Creating server .env file...")
-            fs.writeFileSync("./server/.env", 'NODE_ENV=development\nPORT=3000');
-            await load(100, "Creating server index.js file...")
-            fs.writeFileSync("./server/src/index.js", `const express = require("express");
-            const app = express();
-            const morgan = require("morgan");
-            require("dotenv").config();
-            
-            app.use(morgan("dev"));
-            app.use(express.json());
-            app.use(express.urlencoded({ extended: true }));
-            app.set("PORT", process.env.PORT || 3000);
-            app.use("/", require("./routes/routes"));
-            
-            app.listen(app.get("PORT"), () => {
-                console.log("Server running on port " + app.get("PORT"));
-            });`);
             await load(100, "Creating server routes file...")
             fs.writeFileSync("./server/src/routes/routes.js", `const express = require("express");\nconst router = express.Router();\n\nrouter.get('/', async(req, res) =>{\nres.send('hello world')\n})\n\n module.exports = router;`);
             await load(100, "Creating server controllers file...")
-            fs.writeFileSync("./server/src/controllers/controllers.js" `module.exports = {};`); 
+            fs.writeFileSync("./server/src/controllers/controllers.js", `module.exports = {};`);
             await load(100, "Installing dependencies with npm...")
             await exec(`cd server && npm i express dotenv morgan`);
             console.log(chalk.green("Server project created!"));
+        }
+        if(backend == "nest") {
+            await load(100, "Starting nest project with nest cli")
+            await exec('npm i -g @nestjs/cli')
+            await load(15000, 'Waiting for nest cli to finish installation...');
+            await exec('nest new server')
+        }
+        if(backend == "fastify") {
+            await load(100, "Starting fastify project with fastify cli")
+            await exec('npm i -g fastify')
+            await load(15000, 'Waiting for fastify cli to finish installation...');
+            await exec('fastify new server')
+        }
+        if(backend == "koa") {
+            await load(100, "Starting koa project with koa cli")
+            await exec('npm i -g koa')
+            await load(15000, 'Waiting for koa cli to finish installation...');
+            await exec('koa new server')
         }
     }
 }
